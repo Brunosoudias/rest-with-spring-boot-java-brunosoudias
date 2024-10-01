f4c4d84bef502d17e2799034007a97333cc8f2a5
Bruno Dias <bruno.sousa.d@icloud.com>
2024-10-01 14:08:29 -0300

    First Steps in Kava with Spring Boot 3

---
+++ rest-with-spring-boot-java-brunosoudias/src/test/java/br/com/brunosoudias/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.brunosoudias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
